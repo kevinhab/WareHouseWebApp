84dbec706229f9b2d455bef6016f18fcb047b7c9
Kevin Le <kiet.ltk@outlook.com>
2025-06-18 21:36:17 +0700

    up client test

---
+++ GrpcClientTest/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using WareHouseWebApp.Services;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7052/");
+var client = new InventoryService.InventoryServiceClient(channel);
+
+var reply = await client.GetItemAsync(new ItemRequest { Id = 1 });
+
+Console.WriteLine($"Tên: {reply.Name}, Số lượng: {reply.Quantity}");
